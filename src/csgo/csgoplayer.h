f50452f6818ccc5315ccfca6fe502e723f5903ed
2lag <aidenhelfman@gmail.com>
2023-05-17 15:25:34 -0500

    removing some of those magic numbers

--- src/csgo/csgoplayer.h
+++ src/csgo/csgoplayer.h
@@ -12,12 +12,12 @@ public:
   
   // this doesnt need to be a part of the aimbot.
   VEC3 get_bone_pos( I32 bone_id ) {
-    U32 bonematrix = get<U32>( 0x26a8 );
-
-    // why read 3 times when u can read once?
-    return get<VEC3>( bonematrix + 0x30 * bone_id + 0x0c );
+    return VEC3{
+      csgop->read<F32>( m_dwBoneMatrix( ) + 0x30 * bone_id + 0x0c ),
+      csgop->read<F32>( m_dwBoneMatrix( ) + 0x30 * bone_id + 0x1c ),
+      csgop->read<F32>( m_dwBoneMatrix( ) + 0x30 * bone_id + 0x2c )
+    };
   }
 
-  
   NETVAR( m_iTeam, "DT_CSPlayer", I32 );
 };
